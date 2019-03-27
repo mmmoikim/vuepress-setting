6bb52baa1bb09568844bb3470c0dcc2c1d56290d
Yukyung <yukyung204@interpark.com>
2019-03-27 10:21:56 +0900

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    themeConfig: {
+        sidebar: 'auto',
+        lastUpdated: 'Last Updated',
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+# VUEPRESS-SETTING
+
+## Quick Overview
+
+```sh
+  npm docs:dev
+  npm docs:build
+```
